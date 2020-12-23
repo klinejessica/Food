7570471cd40973fdd12249b59ee1792e1c5cb932
Jessica Kline <kline.jessica@gmail.com>
2020-12-22 18:14:48 -0800

    changes before update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SimpleDirective } from './simple.directive';
 import { AppRoutingModule} from './app-routing.module';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { TimelineModule } from 'primeng/timeline';
 import { ItemService } from './grocery/Service/itemservice';
 import { LoginComponent } from './login/login.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -42,6 +43,7 @@ import { NavigationComponent } from './navigation/navigation.component';
     AppRoutingModule,
      SidebarModule,
      ButtonModule,
+     TimelineModule
     
     
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { TimelineModule } from 'primeng/timeline';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TimelineModule
   ]
 })
 export class HomeModule { }
